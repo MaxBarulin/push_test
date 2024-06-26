552883a27d38bb1f0d2a88f241adaddceaccf245
Max Barulin <barulin.max@gmail.com>
2024-06-26 22:00:07 +0300

    add tests 93%

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+@pytest.fixture()
+def card_number():
+    return '1234567812345678'
+
+
+@pytest.fixture()
+def mask_account():
+    return '73654108430135874305'
+
+
+
+
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,9 +1,20 @@
+import pytest
+
 from src.masks import get_mask_account, get_mask_card_number
+from src.widget import mask_account_card
 
 
-def test_get_mask_account():
-    assert get_mask_account("73654108430135874305") == "**4305"
+def test_get_mask_account(mask_account):
+    assert get_mask_account(mask_account) == "**4305"
 
 
-def test_get_mask_card_number():
-    assert get_mask_card_number("1234567812345678") == "1234 56** **** 5678"
+def test_get_mask_card_number(card_number):
+    assert get_mask_card_number(card_number) == "1234 56** **** 5678"
+
+
+@pytest.mark.parametrize("x, y", [("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
+                                  ("Mastercard 8990922113665229", "Mastercard 8990 92** **** 5229"),
+                                  ("Счет 73654108430135874305", "Счет **4305"),
+                                  ("Счет 73654108430135874416", "Счет **4416")])
+def test_mask_account_card(x, y):
+    assert mask_account_card(x) == y

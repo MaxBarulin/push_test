8ed096d5772175ec91c4689091dbaeea76b8f161
Max Barulin <barulin.max@gmail.com>
2024-07-05 00:15:15 +0300

    добавление декоратора log и его тесты

---
+++ src/decorators.py
@@ -0,0 +1,61 @@
+def log(filename=None):
+    def my_decorator(func):
+        def wrapper(*args, **kwargs):
+            if filename is None:
+                try:
+                    func(*args, **kwargs)
+                    print(f"{func.__name__} ok")
+                except Exception as e:
+                    print(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}")
+                    #print("После выполнения функции")
+            else:
+                if type(filename) == str:
+                    try:
+                        func(*args, **kwargs)
+                        #print("ok")
+                        with open(filename, 'a') as file:
+                            file.write(f"{func.__name__} ok")
+                            file.write("\n")
+
+                    except Exception as e:
+                        with open(filename, 'a') as file:
+                            file.write(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}")
+                            file.write("\n")
+                else:
+                    filename_str = f"{str(filename)}.txt"
+                    try:
+                        func(*args, **kwargs)
+                        # print("ok")
+                        with open(filename_str, 'a') as file:
+                            file.write(f"{func.__name__} ok")
+                            file.write("\n")
+
+                    except Exception as e:
+                        with open(filename_str, 'a') as file:
+                            file.write(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}")
+                            file.write("\n")
+
+        return wrapper
+
+    return my_decorator
+
+
+@log()
+def my_function(x, y):
+    return x / y
+
+@log(filename="mylog.txt")
+def my_function_file(x, y):
+    return x / y
+
+my_function_file(10, 20)
+with open('mylog.txt', 'r') as file:
+    lines = file.readlines()
+    print(lines[-1])
+
+if __name__ == "__main__":
+    #my_function(10, 20)
+    pass
+
+
+
---
+++ tests/test_decorators.py
@@ -0,0 +1,29 @@
+import pytest
+from src.decorators import my_function, my_function_file
+
+
+def test_log_ok(capsys):
+    my_function(10, 20)
+    captured = capsys.readouterr()
+    assert captured.out == "my_function ok\n"
+
+
+def test_log_error(capsys):
+    my_function()
+    captured = capsys.readouterr()
+    assert captured.out == "my_function error: my_function() missing 2 required positional arguments: 'x' and 'y'. Inputs: (), {}\n"
+
+
+def test_log_ok_file():
+    my_function_file(10, 20)
+    with open('mylog.txt', 'r') as file:
+        lines = file.readlines()
+        assert lines[-1] == "my_function_file ok\n"
+
+
+def test_log_error_file():
+    my_function_file()
+    with open('mylog.txt', 'r') as file:
+        lines = file.readlines()
+        assert lines[-1] == "my_function_file error: my_function_file() missing 2 required positional arguments: 'x' and 'y'. Inputs: (), {}\n"
+
